d9d564c72e5587fa38f5a85801e96c4a71c7213b
AJ <alexanderjessediaz@gmail.com>
2021-04-08 13:38:07 -0600

    change color of about me section to match welcome view

--- src/Components/About.js
+++ src/Components/About.js
@@ -37,7 +37,7 @@ export default function About(){
                     />
                     <div className="text-lg flex flex-col justify-center">
                         <h1 className="cursive text-6xl text-green-300 mb-4">
-                            Hey There, I'm{" "}
+                            Hey there, I'm{" "}
                             <span className="text-green-100">{author.name}</span>
                         </h1>
                         <div className="prose lg:prose-xl text-white">
