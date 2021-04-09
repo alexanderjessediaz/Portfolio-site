10193de6374fba3d61b7ae63928d39625e8274c9
AJ <alexanderjessediaz@gmail.com>
2021-04-09 15:34:13 -0600

    typo engineer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ export default function Home(){
 
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"> AJ Diaz: &nbsp;</h1>
-                < h3 className="text-4l text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Software Engnieer</h3>
+                < h3 className="text-4l text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Software Engineer</h3>
             </section>
         </main>
     )
