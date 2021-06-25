61a5328cac5239fb4e5fce863e8424c3657e605a
AJ <alexanderjessediaz@gmail.com>
2021-06-25 16:35:15 -0600

    change the hover color of navbar text to light blue

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -11,19 +11,19 @@ export default function NavBar(){
                         to="/"
                         exact
                         activeClassName="text-white" 
-                        className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                        className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-blue-400 text-4xl font-bold cursive tracking-widest">
                         AJ
                     </NavLink>
                     <NavLink 
                         to="/post" 
-                        className="inline-flex text-lg items-center p-4 my-6 rounded text-red-100 hover:text-green-800"
+                        className="inline-flex text-lg items-center p-4 my-6 rounded text-red-100 hover:text-blue-400"
                         activeClassName="text-green-100 bg-green-700"
                     >   
                         Blog Posts
                     </NavLink>
                     <NavLink 
                         to="/project"
-                        className="inline-flex items-center text-lg p-4 my-6 rounded text-red-100 hover:text-green-800"
+                        className="inline-flex items-center text-lg p-4 my-6 rounded text-red-100 hover:text-blue-400"
                         activeClassName="text-green-100 bg-green-700"
                     >
                         Projects
