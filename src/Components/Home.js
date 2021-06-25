db3f0dfeee94b9e88251c765e64a95be204e2e49
AJ <alexanderjessediaz@gmail.com>
2021-06-25 16:37:37 -0600

    change h1 in home page text to mono from cursive

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -38,7 +38,7 @@ export default function Home(){
                         alt={author.name}
                     />
                     <div className="text-lg flex flex-col justify-center">
-                        <h1 className="cursive text-6xl text-green-300 mb-4">
+                        <h1 className="font-mono text-4xl text-green-300 mb-4">
                             Hey there, I'm{" "}
                             <span className="text-green-100">{author.name}</span>
                         </h1>
