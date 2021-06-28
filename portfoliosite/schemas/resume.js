195c5ec6f296b177cc6abf0a8a4a604e93a88845
AJ <alexanderjessediaz@gmail.com>
2021-06-28 13:57:08 -0600

    made header responsive for mobile

--- portfoliosite/schemas/resume.js
+++ portfoliosite/schemas/resume.js
@@ -1,13 +1,19 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default {
-    name: "resume",
-    title: "Resume",
-    type: "document",
-    fields : [ 
-        {
-            title: "Resume",
-            name: "resume",
-            type: "file"
-        }
-    ]
-};
+// export default {
+//     name: "resume",
+//     title: "Resume",
+//     type: "document",
+//     fields : [ 
+//         {
+//             title: "date",
+//             type: "date",
+//             name: "date"
+//         },
+//         {
+//             title: "Resume",
+//             name: "resume",
+//             type: "file"
+//         }
+
+//     ]
+// };
--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-// import {Home, About, SinglePost, Post, Project} from "./components/Index";
 import Home from "./Components/Home";
-import About from "./Components/About";
 import SinglePost from "./Components/SinglePost";
 import Post from "./Components/Post";
 import Project from "./Components/Project";
 import NavBar from "./Components/NavBar";
-
 
 function App() {
   return (
@@ -14,7 +11,6 @@ function App() {
     <NavBar />
       <Switch>
         <Route component={Home} path='/' exact/>
-        <Route component={About} path='/about'/>
         <Route component={SinglePost} path='/post/:slug'/>
         <Route component={Post} path='/post'/>
         <Route component={Project} path='/project'/>
