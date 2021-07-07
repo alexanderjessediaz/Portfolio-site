c9000dac48b84fa5539df6dc2c75435722b7a274
AJ <alexanderjessediaz@gmail.com>
2021-07-07 10:51:34 -0600

    add key to article in Project for each project in list

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from "react";
 import sanityClient from "../client.js";
 
-
 export default function Resume(){
 
     const [resumeData, setResume] = useState(null);
@@ -23,20 +22,21 @@ export default function Resume(){
             .catch(console.error)
     }, []);
 
-    const seeData = () => {
-        console.log("resumeData:" ,resumeData[1])
-    }
-
     if (!resumeData) return <div>Loading...</div>;
 
     return (
-        <main>
-            {seeData()}
-            <section>
+        <main className="min-h-screen p-12">
+            <span 
+                className="block relative h-full flex justify-center items-end pr-4 pb-4">
+                    <h1 className="text-5xl flex justify-center cursive p-4 bg-green-700 text-red-100 rounded bg-opacity-75">Resume</h1>
+                </span>
+            <section className="container mx-auto">
+                <header className="relative flex items-center justify-center" >
                 <img
                     src={resumeData[1].mainImage.asset.url}
                     alt={resumeData.title}
                 />
+                </header>
             </section>
         </main>
     )
