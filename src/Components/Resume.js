84bc1d054e3d37b632e632308a265bf2fde420bd
AJ <alexanderjessediaz@gmail.com>
2021-07-09 09:12:25 -0600

    replace embed with object for better web support. Add a link to resume for mobile users who don't have a pdf plug-in

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -12,11 +12,28 @@ export default function Resume(){
                     >Resume</h1>
             </span>
             <div className="flex justify-center p-4">
-                <embed
+                <p className="text-center">If your web browser doesn't have a pdf plugin you can view it here <br></br> 
+                    <a
+                        className="flex justify-center hover:underline text-white"
+                        href="https://docdro.id/Lv8KILj"
+                        alt="AJ Diaz's Resume"
+                        target="_blank"
+                        rel="noreferrer"
+                    >AJ Diaz's Resume</a></p>
+            </div>
+            <div className="flex justify-center p-4">
+                <object
+                    className="min-h-full w-auto md:w-3/4 lg:w-2/4"
+                    data={myResume}
+                    type="application/pdf"
+                    style={{height:"50rem"}}
+                >
+                </object>
+                {/* <embed
                     className="min-h-full w-auto md:w-3/4 lg:w-2/4"
                     src={myResume}
                     style={{height:"50rem"}}
-                    ></embed>
+                ></embed> */}
             </div>
         </main>
     )
